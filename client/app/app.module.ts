97d49da383ba7e82f6c6c33a65217e9b5059fda8
Lucas <lucasfelipesantos2012@gmail.com>
2017-06-11 19:29:05 -0300

    Aprendendo sobre pipe (filtros), requisições http e rotas

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { PainelModule } from './painel/painel.module';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { routing } from './app.routes';
 
 @NgModule({ // Modulo do angular
-    imports: [ BrowserModule, FotoModule ], // Dependencia de modulos da minha aplicação principal
-    declarations: [ AppComponent ], // o que faz parte do modulo.
-    bootstrap: [ AppComponent ] // quem eu quero iniciar.
+    imports: [
+        BrowserModule, 
+        FotoModule, 
+        HttpModule, 
+        PainelModule,
+        routing
+    ], // Dependencia de modulos da minha aplicação principal
+    declarations: [AppComponent, CadastroComponent, ListagemComponent], // o que faz parte do modulo.
+    bootstrap: [AppComponent] // quem eu quero iniciar.
 })
-export class AppModule {}
+export class AppModule { }
